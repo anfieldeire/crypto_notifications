3fefaca5dd5744abc89d9df9f99b595ca3ed8fb4
anfieldeire <gareth.hasson@gmail.com>
2020-12-02 18:42:46 -0500

    Add files via upload

--- twilio_message.py
+++ twilio_message.py
@@ -4,16 +4,58 @@ from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-account_sid = os.environ['TWILIO_ACCOUNT_SID']
-auth_token = os.environ['TWILIO_AUTH_TOKEN']
 
-client = Client(account_sid, auth_token)
+def message_config(find_percentage):
 
-message = client.messages \
-                .create(
-                     body="TEST MESSAGE",
-                     from_='+1',
-                     to='+1'
-                 )
+    for each_list in find_percentage:
+        # print("each list {}".format(each_list))
+        user_data = each_list[0]
+        # print("user data {}".format(user_data))
 
-print(message.sid)
+        main_body = ''
+
+        for each_dict in each_list[1:]:
+            # print(each_dict)
+            # print(each_dict['current_price'])
+
+            main_body = main_body + "\nCoin {} is {} {}%, in 24hrs, current price is now ${}" \
+                                    "\nCoin Subtotal went from ${} to ${}".format(each_dict['id'],
+            each_dict['direction'], each_dict['price_change_24'], each_dict['current_price'],
+            each_dict['coinsubtotal_24hago'], each_dict['coinsubtotal_now'])
+
+            greeting = 'Hello {}. Crypto Alerts Notification:'.format(user_data['name'])
+            number = user_data['phone']
+
+        print(greeting)
+        print(main_body)
+        print(number)
+        return greeting, main_body, number
+
+
+def send_message():
+
+    greeting, main_body, number = message_config()
+    print("from send message")
+    print(greeting)
+    print(main_body)
+    print(number)
+
+    account_sid = os.environ['TWILIO_ACCOUNT_SID']
+    auth_token = os.environ['TWILIO_AUTH_TOKEN']
+
+    client = Client(account_sid, auth_token)
+
+    message = client.messages \
+                    .create(
+                         body="TEST MESSAGE",
+                         from_='+1',
+                         to='+1'
+                     )
+
+    print(message.sid)
+
+#send_message()
+
+
+if __name__ == '__main__':
+    message_config()
